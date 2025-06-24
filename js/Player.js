3102d0795675e9b46d71746d093b2ac04e668873
DondaineMathieu <dondaine.mathieu@gmail.com>
2025-06-24 15:16:48 +0200

    make even more css to be closer to design including console style & player stats bar

--- js/Player.js
+++ js/Player.js
@@ -23,24 +23,29 @@ class Player {
         const li = document.createElement('li');
         if ('enemy' == this.type) {
             li.innerHTML = 
-                `<i>V</i>
-                <h4>${this.name}</h4>`
+                `<h4>
+                    <i>V</i>
+                    ${this.name}
+                </h4>`
         } else {
             li.innerHTML =
-                `<i>V</i>
-                <h4>${this.name}</h4>
-                <div class="entity_stats player_stats">
-                    <div class="entity_stat player_stat --health">
-                        PV
-                        <span class="entity_stat_bar player_stat_bar"></span>
+                `
+                <h4>
+                    <i>V</i>
+                    ${this.name}
+                </h4>
+                 <div class="entity_stats player_stats">
+                    <div class="entity_stat player_stat">
+                        <span class="entity_stat_label player_stat_label">PV</span>
+                        <span class="entity_stat_bar player_stat_bar --health"></span>
                     </div>
-                    <div class="entity_stat player_stat --armor">
-                        Armor
-                        <span class="entity_stat_bar player_stat_bar"></span>
+                    <div class="entity_stat player_stat">
+                        <span class="entity_stat_label player_stat_label">Armor</span>
+                        <span class="entity_stat_bar player_stat_bar --armor"></span>
                     </div>
-                    <div class="entity_stat player_stat --mana">
-                        Mana
-                        <span class="entity_stat_bar player_stat_bar"></span>
+                    <div class="entity_stat player_stat">
+                        <span class="entity_stat_label player_stat_label">Mana</span>
+                        <span class="entity_stat_bar player_stat_bar --mana"></span>
                     </div>
                 </div>`
         }
